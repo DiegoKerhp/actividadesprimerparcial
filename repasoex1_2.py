863040f3aa23db94938175a77cd5232aeaf53184
DiegoKerhp <145036304+DiegoKerhp@users.noreply.github.com>
2023-09-19 09:07:37 -0600

    Create repasoex1_2.py

---
+++ repasoex1_2.py
@@ -0,0 +1,10 @@
+def promedio_ de _notas ():
+print ("Ingresa tu primer número")
+x=input ( )
+print( "Ingresa tu segundo número")
+y=input ()
+print "Ingresa tu tercer número")
+z=input ( )
+Pr=(int(x)+int(y)+int(z))/3
+print( "Tu promedio es", Pr)
+promedio_de_notas()
